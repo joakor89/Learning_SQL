8cea45a70453e57715f9c36ac6b5bbdb0a26321a
Joaquin Romero Flores <jromerof30@gmail.com>
2024-03-26 07:27:44 -0600

    Partitioning, sharding & hadoop

--- chap_17/large_DB.sql
+++ chap_17/large_DB.sql
@@ -147,11 +147,71 @@ SELECT CONCAT('# of rows in H3 = ', count(*)) partition_rowcount
 SELECT CONCAT('# of rows in H4 = ', count(*)) partition_rowcount
 	FROM sales PARTITION (h4) UNION ALL
 
-# cOMPOSITE partitioning
+# Composite partitioning
 
+###
+DROP TABLE sales;
+###
 
+CREATE TABLE sales
+	(sale_id INT NOT NULL,
+     cust_id INT NOT NULL,
+     store_id INT NOT NULL,
+     sale_date DATE NOT NULL,
+     geo_region_cd VARCHAR(6) NOT NULL,
+     amount DECIMAL(9, 2)
+	)
 
+PARTITION BY RANGE (yearweek(sale_date))
+	SUBPARTITION BY HASH (cust_id)
+    (PARTITION s1 VALUES LESS THAN (202002)
+    (SUBPARTITION s1_h1,
+     SUBPARTITION s1_h2,
+     SUBPARTITION s1_h3,
+     SUBPARTITION s1_h4),
+	PARTITION s2 VALUES LESS THAN (202003)
+    (SUBPARTITION s2_h1,
+     SUBPARTITION s2_h2,
+     SUBPARTITION s2_h3,
+     SUBPARTITION s2_h4),
+	PARTITION s3 VALUES LESS THAN (202004)
+    (SUBPARTITION s3_h1,
+     SUBPARTITION s3_h2,
+     SUBPARTITION s3_h3,
+     SUBPARTITION s3_h4),
+	PARTITION s4 VALUES LESS THAN (202005)
+    (SUBPARTITION s4_h1,
+     SUBPARTITION s4_h2,
+     SUBPARTITION s4_h3,
+     SUBPARTITION s4_h4),
+     PARTITION s5 VALUES LESS THAN (202006)
+    (SUBPARTITION s5_h1,
+     SUBPARTITION s5_h2,
+     SUBPARTITION s5_h3,
+     SUBPARTITION s5_h4),
+	PARTITION s999 VALUES LESS THAN (MAXVALUE)
+    (SUBPARTITION s999_h1,
+     SUBPARTITION s999_h2,
+     SUBPARTITION s999_h3,
+     SUBPARTITION s999_h4)
+);
 
+INSERT INTO sales 
+	VALUES
+    (1, 1, 1, '2020-01-18', 1.1), (2, 3, 4, '2020-02-07', 1.2),
+    (3, 17, 5, '2020-01-19', 1.3), (4, 23, 2, '2020-02-08', 1.4),
+    (5, 56, 1, '2020-01-20', 1.6), (6, 77, 5, '2020-02-09', 1.7),
+    (7, 122, 4, '2020-01-21', 1.8), (8, 153, 1, '2020-02-10', 1.9),
+    (9, 179, 5, '2020-01-22', 2.0), (10, 244, 2, '2020-02-11', 2.1),
+    (11, 263, 1, '2020-01-23', 2.2), (12, 312, 4, '2020-02-12', 2.3),
+    (13, 346, 2, '2020-01-24', 2.4), (14, 389, 3, '2020-02-13', 2.5),
+    (15, 472, 1, '2020-01-25', 2.6), (16, 502, 1, '2020-02-14', 2.7);
+
+SELECT *
+	FROM sales PARTITION (s3);
+    
+SELECT *
+	FROM sales PARTITION (s3_H3);
 
 
 
